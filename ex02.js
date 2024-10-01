42e025b114a8f01b19d99ea55cfc8c0aec1cee41
Tomoya <flyingfox193807@gmail.com>
2024-10-01 09:54:08 -0700

    finished ex02.js

--- ex02.js
+++ ex02.js
@@ -2,4 +2,14 @@
 // containing the elements that are in the first array but not in the second array.
 // Example: arrayDifference([1, 2, 3], [2, 3, 4]) should return [1].
 
-console.log(arrayDifference([1, 2, 3], [2, 3, 4])); // Expected output: [1]
+function arrayDifference(arrays) {
+    let result = []
+    for (i = 0; i < arrays[0].length; i++) {
+        if (arrays[1].includes(arrays[0][i]) == false) {
+            result.push(arrays[0][i])
+        }
+    }
+    return result
+}
+
+console.log(arrayDifference([[1, 2, 3], [2, 3, 4]])); // Expected output: [1]

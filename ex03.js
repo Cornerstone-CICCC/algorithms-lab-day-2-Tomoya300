e76c21786f0bb3a7f641cb7365c95e8af52714e5
Tomoya <flyingfox193807@gmail.com>
2024-10-01 10:21:48 -0700

    finished ex03.js

--- ex03.js
+++ ex03.js
@@ -2,4 +2,16 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+function findMissingNumber(array) {
+    let missing = 0
+    for (i = 0; i < array.length - 1; i++) {
+        if (array[i] + 1 === array[i + 1]) {
+            missing += 0
+        } else {
+            missing += array[i] + 1
+        }
+    }
+    return missing
+}
+
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3

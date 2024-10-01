8af07b0ec45a33d6656158b3cec686fdea61c0e3
Tomoya <flyingfox193807@gmail.com>
2024-10-01 12:03:55 -0700

    finished ex06.js

--- ex06.js
+++ ex06.js
@@ -3,4 +3,21 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+function getQueryParams(url) {
+    let result = {}
+    let arr1 = url.split("?") // [ 'https://example.com', 'search=test&page=2' ]
+    let arr2
+    if (arr1.length > 1) {
+        arr2 = arr1[1].split("&")
+    }
+    for (i = 0; i < arr2.length; i++) {
+        let sub = arr2[i].split("=")
+        result[sub[0]] = sub[1]
+    }
+
+    return result
+}
+
+
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }

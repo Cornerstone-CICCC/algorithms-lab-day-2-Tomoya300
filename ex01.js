5e554a2a3d7c5a6c7528d886b06be81ccfd36730
Tomoya <flyingfox193807@gmail.com>
2024-10-01 09:40:49 -0700

    finished ex01.js

--- ex01.js
+++ ex01.js
@@ -2,4 +2,18 @@
 // with the first letter of each word capitalized.
 // Example: capitalizeWords("hello world") should return "Hello World".
 
-console.log(capitalizeWords("hello world")); // Expected output: "Hello World"
+function capitalizeWords(string) {
+    let arr1 = string.split(" ")
+    let result = ""
+    for (i = 0; i < arr1.length; i++) {
+        if (arr1[i][0]) {
+            result += arr1[i][0].toUpperCase() + arr1[i].slice(1)
+        }
+        if (i < arr1.length-1) {
+            result += " "
+        }
+    }
+    return result
+}
+
+console.log(capitalizeWords("how kind of you to let me come")); // Expected output: "Hello World"

ee42c3e9b8cb10e628d954b4444ee1e496d2d8e1
Tomoya <flyingfox193807@gmail.com>
2024-10-01 10:50:52 -0700

    finished ex05.js

--- ex05.js
+++ ex05.js
@@ -2,4 +2,23 @@
 // and returns the number of consonants in the string.
 // Example: countConsonants("hello world") should return 7.
 
+function countConsonants(string) {
+    let arr1 = string.split(" ")
+    let arr2 = []
+    let output = 0
+    for (i = 0; i < arr1.length; i++) {
+        for (k = 0; k < arr1[i].length; k++) {
+            arr2.push(arr1[i][k])
+        }
+    }
+    for (g = 0; g < arr2.length; g++) {
+        if (arr2[g] === "a" || arr2[g] === "i" || arr2[g] === "e" || arr2[g] === "o" || arr2[g] === "u") {
+            output += 0
+        } else {
+            output += 1
+        }
+    }
+    return output
+}
+
 console.log(countConsonants("hello world")); // Expected output: 7

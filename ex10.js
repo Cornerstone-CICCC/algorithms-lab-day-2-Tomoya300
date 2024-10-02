7eb72ac40f165a6ef124fed596a9840915d3a153
Tomoya <flyingfox193807@gmail.com>
2024-10-01 21:39:07 -0700

    finished ex10.js

--- ex10.js
+++ ex10.js
@@ -2,4 +2,15 @@
 // with words separated by spaces or underscores, and returns the string in camelCase format.
 // Example: convertToCamelCase("hello_world") should return "helloWorld".
 
+function convertToCamelCase(string) {
+    let arr1 = string.split("_")
+    let result = arr1[0]
+    for (i = 1; i < arr1.length; i++) {
+        if (arr1[i][0]) {
+            result += arr1[i][0].toUpperCase() + arr1[i].slice(1)
+        }
+    }
+    return result
+}
+
 console.log(convertToCamelCase("hello_world")); // Expected output: "helloWorld"

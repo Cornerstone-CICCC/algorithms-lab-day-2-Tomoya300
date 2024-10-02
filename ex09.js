ed2f0d805e5e47093306a7ff28c018bfcb394abb
Tomoya <flyingfox193807@gmail.com>
2024-10-01 21:06:54 -0700

    finished ex09.js

--- ex09.js
+++ ex09.js
@@ -3,4 +3,14 @@
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
 
-console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
+function sortByProperty(array, condition) {
+    function comparison(a, b) {
+        if (a[condition] > b[condition]) return 1
+        if (a[condition] < b[condition]) return -1
+        return 0
+    }
+    let result = array.sort(comparison)
+    return result
+}
+
+console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}, {name: "Ethan", age: 21}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]

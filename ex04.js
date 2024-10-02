e140b57f3dfae83ad0a7bde8e8253a93abf99adf
Tomoya <flyingfox193807@gmail.com>
2024-10-01 17:38:55 -0700

    finished ex04.js

--- ex04.js
+++ ex04.js
@@ -2,4 +2,22 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
-console.log(mostFrequentChar("javascript")); // Expected output: "a"
+function mostFrequentChar(string) {
+    let obj1 = {}
+    let largest = 0
+    let maxch = ""
+    for (i = 0; i < string.length; i++) {
+        if (!obj1[string[i]]) {
+            obj1[string[i]] = 1
+        } else if (obj1[string[i]]) {
+            obj1[string[i]] += 1
+        }
+        if (obj1[string[i]] > largest) {
+            largest = obj1[string[i]]
+            maxch = string[i]
+        }
+    }
+    return maxch
+}
+
+console.log(mostFrequentChar("javascript")); // Expected output: "c"
